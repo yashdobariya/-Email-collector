51c409abbc5d661bec625ce388d0bac2ec5301f1
Yash Dobariya <60248999+yashdobariya@users.noreply.github.com>
2020-11-07 21:55:26 +0530

    initial commit

---
+++ Email collector.py
@@ -0,0 +1,288 @@
+
+#                     Email collector
+
+# here you can use your string to find eamil address
+# Example string
+str='''
+Save Time & Supercharge Your Gmail with Right Inbox
+DOWNLOAD FOR FREE
+Features
+Pricing
+Blog
+Add to Gmail
+AUGUST 1, 2020 • 
+BLOG
+0 
+40 Ideas for Creating a Professional Email Address
+ DAVID CAMPBELLSHARE  
+
+Your email address is like your online business card. For many prospects, it’s the first thing they see when they interact with you, and it can have a huge impact on the kind of impression you make. That’s why it’s crucial that you spend time on creating a professional email address.
+
+Forty-seven percent of recipients base their decision to open your email on subject line alone, so it stands to reason that your email address can also have an outsized impact on open rates.
+
+So while you may want an address like QueenOfTheRealm, HoppyBeerLover or SpoiledRotten123 for your personal predilections, save those for your personal email account. When you’re communicating with potential clients and current customers, it’s critical to have an email address that inspires confidence.
+View in Chrome Web Store
+Level Up Your Gmail Account with Right Inbox
+
+Email Reminders, Email Tracking, Email Follow-Ups & more features
+
+
+Best practices for picking the perfect professional email address
+The best email addresses keep things simple. While using your name seems ideal, many simple first-last name combinations may have already been allocated to other digital users. Or, you may have a lengthy name or one that varies from traditional spelling formats, both of which can potentially create issues.
+
+1. Keep things easy to remember
+With so many people conducting business via the tiny screens of their mobile devices, misspelling an address is common, even if it’s something that would generally seem easy to spell correctly.
+
+If you require your business contacts to remember something with a long string of letters and numbers, they may find it’s too difficult to contact you and move on to someone with an easier address.
+
+2. Make it pronounceable
+If you call or converse with someone and mention your email address, he or she should be able to accurately write or repeat the spelling of your username without having to ask you for clarification.
+
+You’ll have to read your email address aloud often in business, so make sure it’s easy and doesn’t require a lot of explanation.
+
+3. Choose your own domain
+If you’re using an email service, Gmail is considered more up-to-date and relevant than AOL, for example – and this choice can color how recipients view your business. While there may be other great providers outside well-known ones like Gmail, it doesn’t do your business any favors to go with a technically-proficient platform that has an impossible-to-spell-or-remember name.
+
+A personalized domain for your business can also be a good choice if you’re able to secure one. The same guidelines apply to creating your URL: make sure it’s memorable, spellable, and pronounceable.
+
+Why Having Your Own Domain is Important?
+
+Builds trust and credibility
+Gives you and your business a more professional look
+Protect copyrights and trademarks
+Increases brand awareness
+Additional Reading: How to Change Your Gmail Address – Everything You Need to Know
+
+
+
+We’ve pulled together a few ideas for email address combinations below. Take a look, then try out a few to see which one fits your business best:
+
+Table of Contents:	
+Name Combinations
+Name Modifications
+Inverted Name
+Highlighting Qualifications
+Feature your Location
+Let Form Follow Function
+Do Something Creative
+Highlight Your Brand
+Sales@ Alternatives
+Inquiry@ Alternatives
+Frequently Asked Questions (FAQs)
+Level up your Email Account
+Conclusion
+Name Combinations
+If you’re in luck, your first and last name combination will be available. If it’s not, these other options might fit the bill:
+
+1. First initial + last name = CWales
+
+2. First name + middle name + last name (this one can be more effective for people who have chosen to make a name switch from a maiden name to a partner’s last name, because their maiden name may be more familiar/common) = CatherineMiddletonWales
+
+3. First name + middle initial + last name = CatherineMWales
+
+4. Nickname + last name (choose this if your nickname is common and professional; something like “Squee,” for example, is neither) = KateWales
+
+5. Last name + first name (inverting keeps it simple and gives you additional options to find an address that works) = WalesCatherine
+
+6. First initial + middle initial + last name = CMWales
+
+7. First initial + middle initial + last initial = CMW
+
+8. First name + last initial = CatherineW
+
+Name Modifications
+If your name has been taken, or if it’s too long to fit the required email length, there are plenty of ways you can play with your name to make it fit.
+
+9. Shorten your name – CathWales
+
+10. Include shortened middle name – CathMidWales
+
+Inverted Name
+If your first name and last name combination isn’t available, you might consider putting your last name first. If you go this route, and especially if you have an ambiguous name, make sure you include a clear email signature so that contacts know which name to refer to you by.
+
+11. Last name + first initial + middle initial = WalesCM
+
+12. Last name + first name + middle initial = WalesCatherineM
+
+Highlighting Qualifications
+Overemphasizing your qualifications can be a bit touchy; too much emphasis on a specific title can seem obnoxious, and using industry-specific jargon can be hard to remember. However, in some situations, using a degree or title you’ve earned can make sense:
+
+13. Job title + name (this works well when your job title is closely tied with your role, like a doctor or counselor) = DrSmith, ProfessorJones
+
+14. Job title only = JavaDeveloper
+
+15. Qualifications + name (this one fits if you’re working in a profession that’s closely tied with your qualification; if you have a qualification that is not well-known to the public, it can just look like alphabet soup when they review it) = RossGellarPhD
+
+Feature your Location
+If your business is tied to a specific area or region, or if you’re a branch or franchise of a larger company, it can make sense to include your location in your email ID:
+
+16. Name + city = ShawnSantaBarbara
+
+17. Name + state = ShawnCalifornia
+
+18. First name + company = ShawnPsych
+
+19.Full name + company = ShawnSpencerPsych
+
+20. Company + city = PsychSantaBarbara
+
+Let Form Follow Function
+If you’re using your email address to attract inquiries on your website, it can make sense to set up an address that details exactly what they’re contacting you about. For example, a journalist may not be interested in emailing an address that sounds sales-y, while a typical customer probably wouldn’t understand why they’d need to connect with a PR manager:
+
+21. Department = Sales
+
+22. Role = MediaInquiries
+
+23. General = Questions, Info
+
+24. Name + Role (this can be a great option for personalizing the experience a bit more, but make sure to stick to the easy-to-spell-and-pronounce guidelines) = JimSales or AngelaAccounting
+
+25. Community connection = Outreach, Sponsorship, Donations
+
+26. Job searchers = Careers, WorkWithUs
+
+Do Something Creative
+If your brand has a unique niche, or if you use humor as a selling point, it can make sense to have an email address that’s a little more quirky and interesting.
+
+However, if you choose to go this route, make sure the address you choose is on-brand for your business. If you’re selling a serious product like data security or life insurance, contacts that you’re cold emailing may not respond well to an address like “HackerStopper” or “PushingDaisies”:
+
+27. Founder’s email (this doesn’t have to be monitored 24/7 by the founder, but can give the impression of availability and connectedness, with emails escalated up as appropriate) = BenandJerry, JeffCEO
+
+28. Greeting = Hello, Howdy, Bonjour
+
+29. Core value = Empathy
+
+30. Company name + vision = ManpowerWinWork
+
+31. Corporate Mascot = KeeblerElves, GEICOGecko
+
+32. Play on words = MOOsletter
+
+Highlight Your Brand
+This depends on the name of your domain. If your domain is your brand name, then a [brand name]@[brand name].com email address would be too repetitive. However, if your domain name isn’t taken up by your business or brand name, then feel free to include it. 
+
+33. Use your brand or business name = [SpecialtySweaters]@domain.com
+
+Sales@ Alternatives
+To come across as more approachable to your clients and customers, you can spice up the usual sales email address by using a word that reflects what you’re selling. For example, if you’re a business that helps companies grow, then “growth” would work well. Or if you’re a digital agency helping businesses to be found online, then “visibility” might be better. 
+
+34. Growth = growth@company.com
+
+35. Visibility = visibility@company.com
+
+36. Profits = profits@company.com
+
+Inquiry@ Alternatives
+The usual inquiry email address can be changed up to suit the service you’re offering contacts when they reach out. Is it help, support, information? Whatever it is, use this as your contact address and appeal to those you’re trying to help. 
+
+37. Help = help@company.com
+
+38. Support = support@company.com
+
+39. Info = info@company.com
+
+40. Question = questions@company.com
+
+Frequently Asked Questions (FAQs)
+What Does a Professional Email Address Cost?
+While some email services advertise being free, few truly cost nothing. That said, if you already pay for web hosting, your domain name will often come with a free business email. Web hosting companies can cost as little as $3 per month.
+
+What Should I Include in my Email Signature?
+Your professional email signature should include your first and last name, job title, company (with website URL), and contact information. Your company may also provide a logo and/or stipulate that you link to their social profiles (or you may be able to link to your own). You may also want to consider including a call-to-action in your signature. 
+
+Which is better: G Suite or Microsoft 365?
+G Suite (from $6 per user per month) is fresh and user-friendly, allowing greater collaboration than Microsoft 365. It appeals to individuals/businesses who have web hosting covered but need a business email address. It’s incredibly scalable, with no limit to the number of users regardless of the plan. 
+
+Microsoft 365 (from $5 per user per month) is better suited to Windows-based companies. It offers more functionality than G Suite (e.g. it lets you sort and group emails) and provides greater storage on its basic plan. 
+
+What are the Alternatives to G Suite or Microsoft 365?
+Zoho Email is great for companies on a tight budget that already have a domain name. It provides up to five free business email accounts per domain.  Bluehost is perfect for those using WordPress but who need web hosting with a free domain and business email. 
+
+Level up your Email Account
+Once you’ve created your email address it’s now time to start getting the most from your account. If you decided to go with Gmail as your email provider than you made the right move. One of the major benefits of having a Gmail account is the free add-ons or extensions you can add to supercharge your account. Right Inbox is a free email productivity extension that superchargers your emailing efforts, with such features as:
+
+Email Tracking: Find out who reads and clicks your emails, how many times, and when. Real-time insights on who reads your emails and who’s ignoring you.
+
+
+
+Email Follow-Ups: Set up follow-ups that automatically send if your prospect doesn’t reply. You can forget to follow up, because Right Inbox won’t.
+
+
+
+Email Templates: Turn your most effective emails in templates you can insert and optimize with one click. Save time while adding consistency to your email communication.
+
+
+DOWNLOAD RIGHT INBOX FOR FREE HERE
+
+Conclusion
+Once you have your perfect email address in place, don’t forget to set your display name. Choose something that makes sense to the person opening the email, like your name or a combination of your name and business name.
+
+After that’s in place, you’re ready to start sending out those emails. Check out this list of great email subject lines to help you get started with moving your email campaign forward.
+
+ Popular Posts
+
+What Does Queued Mean in Gmail?
+
+40 Ideas for Creating a Professional Email Address
+
+How to Start an Email & 50 Email Greetings
+
+How to Search Gmail by Date
+
+How to Change Default Gmail Account
+View Updates
+View Templates
+INSTALLS IN 30 SECONDS — WORKS WITH CHROME, SAFARI AND FIREFOX
+Start spending less time in your inbox
+Add to Gmail It's free!
+
+Passionate about your email productivity.
+Product
+Gmail Send Later
+Email Reminders
+Email Notes
+Recurring Emails
+Email Signatures
+Email Templates
+Email Sequences
+Gmail CRM Integration
+Embedded GIFs in Gmail
+Email Tracking
+Feature Request
+Mail Merge
+Right Inbox
+Product
+Pricing
+Blog
+Teams
+Support
+Terms and Conditions
+Privacy Policy
+Contact
+Gmail & G-Suite Update History
+How We Compare
+Boomerang
+FollowUp.cc
+Bananatag
+Yesware
+Mixmax
+International
+German
+Japanese
+French
+Italian
+Spanish
+Gmail is a registered trademark of Google. Right Inbox is not affiliated with Google or Gmail
+
+JOIN THOUSANDS OF OTHER
+HIGH PERFORMERS
+
+Use the Right Inbox extension and get more out of Gmail.
+
+VIEW IN CHROME WEB STORE
+Learn More
+'''
+
+import re
+email= re.findall(r'\w+@\S+\w',str)
+print(email)
